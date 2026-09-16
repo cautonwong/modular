cbce8e4dd049ff5029f0dddffd1a87594c15939d
cautonwong <992878191@qq.com>
2026-09-16 09:34:02 +0800

    board: define IRQ-only event forwarding boundary

---
+++ board/example/include/example/board.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_EXAMPLE_H
+#define BOARD_EXAMPLE_H
+
+#include "edge/event.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void board_example_init(edge_event_sink_t *sink);
+void board_example_irq_uart0_rx(uint32_t byte_count);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
