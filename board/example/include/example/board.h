fc125c696badb05f3dcf066255f1296c276c1d37
cautonwong <992878191@qq.com>
2026-09-16 14:58:28 +0800

    test: expose board timer IRQ hook for QEMU smoke

--- board/example/include/example/board.h
+++ board/example/include/example/board.h
@@ -10,6 +10,8 @@ extern "C" {
 
 void board_example_init(edge_event_sink_t *sink);
 void board_example_irq_uart0_rx(uint32_t byte_count);
+void board_example_irq_timer0(void);
+void board_example_qemu_timer0_init(void);
 
 #ifdef __cplusplus
 }
