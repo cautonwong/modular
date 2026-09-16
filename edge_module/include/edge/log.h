47a6932be5fdfd721d6aa16f310f34cadf08fd00
cautonwong <992878191@qq.com>
2026-09-16 09:40:05 +0800

    architecture: add injected logging port contract

---
+++ edge_module/include/edge/log.h
@@ -0,0 +1,16 @@
+#ifndef EDGE_LOG_H
+#define EDGE_LOG_H
+
+typedef enum edge_log_level {
+    EDGE_LOG_DEBUG = 0,
+    EDGE_LOG_INFO = 1,
+    EDGE_LOG_WARN = 2,
+    EDGE_LOG_ERROR = 3
+} edge_log_level_t;
+
+typedef struct edge_log_port {
+    void (*write)(void *self, edge_log_level_t level, const char *message);
+    void *self;
+} edge_log_port_t;
+
+#endif
