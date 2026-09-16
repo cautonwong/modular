06f1880f3cb8399a417a31d1ec70daa97537501d
cautonwong <992878191@qq.com>
2026-09-16 10:39:03 +0800

    fix: point Cortex-M vector reset to startup

--- .github/arm/startup.c
+++ .github/arm/startup.c
@@ -1,11 +1,12 @@
 #include <stdint.h>
 
 extern int main(void);
+extern void _start(void);
 
 __attribute__((used, section(".isr_vector")))
 const uintptr_t edge_vector_table[] = {
     0x20004000u,
-    (uintptr_t)main,
+    (uintptr_t)_start,
 };
 
 void _start(void)
