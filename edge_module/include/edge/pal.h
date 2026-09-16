a893ba2516747f985618af29f59be8e34a7ca30d
cautonwong <992878191@qq.com>
2026-09-16 14:34:32 +0800

    arch: add PAL contract

---
+++ edge_module/include/edge/pal.h
@@ -0,0 +1,32 @@
+#ifndef EDGE_PAL_H
+#define EDGE_PAL_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void (*edge_pal_critical_fn)(void *self);
+typedef uint64_t (*edge_pal_now_fn)(void *self);
+typedef bool (*edge_pal_in_isr_fn)(void *self);
+typedef void (*edge_pal_barrier_fn)(void *self);
+typedef void (*edge_pal_isr_fn)(void *self);
+
+typedef struct edge_pal_port {
+    edge_pal_critical_fn critical_enter;
+    edge_pal_critical_fn critical_exit;
+    edge_pal_barrier_fn memory_barrier;
+    edge_pal_now_fn monotonic_ticks;
+    edge_pal_in_isr_fn in_isr;
+    edge_pal_isr_fn isr_enter;
+    edge_pal_isr_fn isr_exit;
+    void *self;
+} edge_pal_port_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
