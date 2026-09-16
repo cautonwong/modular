674475ba550f8a21e44593caf9340fbb286aa351
cautonwong <992878191@qq.com>
2026-09-16 14:58:23 +0800

    test: define board timer interrupt event

--- edge_module/include/edge/events.h
+++ edge_module/include/edge/events.h
@@ -9,17 +9,23 @@
 
 #define EDGE_EVT_UART0_RX (EDGE_EVT_BOARD_BASE + 0x01u)
 #define EDGE_EVT_UART0_TX_DONE (EDGE_EVT_BOARD_BASE + 0x02u)
+#define EDGE_EVT_BOARD_TIMER0 (EDGE_EVT_BOARD_BASE + 0x03u)
 #define EDGE_EVT_DLT645_RX (EDGE_EVT_DLT645_BASE + 0x01u)
 #define EDGE_EVT_DLMS_RX (EDGE_EVT_DLMS_BASE + 0x01u)
 #define EDGE_EVT_RELAY_CHANGED (EDGE_EVT_RELAY_BASE + 0x01u)
 
 _Static_assert(EDGE_EVT_UART0_RX != EDGE_EVT_UART0_TX_DONE, "duplicate event ID");
+_Static_assert(EDGE_EVT_UART0_RX != EDGE_EVT_BOARD_TIMER0, "duplicate event ID");
 _Static_assert(EDGE_EVT_UART0_RX != EDGE_EVT_DLT645_RX, "duplicate event ID");
 _Static_assert(EDGE_EVT_UART0_RX != EDGE_EVT_DLMS_RX, "duplicate event ID");
 _Static_assert(EDGE_EVT_UART0_RX != EDGE_EVT_RELAY_CHANGED, "duplicate event ID");
+_Static_assert(EDGE_EVT_UART0_TX_DONE != EDGE_EVT_BOARD_TIMER0, "duplicate event ID");
 _Static_assert(EDGE_EVT_UART0_TX_DONE != EDGE_EVT_DLT645_RX, "duplicate event ID");
 _Static_assert(EDGE_EVT_UART0_TX_DONE != EDGE_EVT_DLMS_RX, "duplicate event ID");
 _Static_assert(EDGE_EVT_UART0_TX_DONE != EDGE_EVT_RELAY_CHANGED, "duplicate event ID");
+_Static_assert(EDGE_EVT_BOARD_TIMER0 != EDGE_EVT_DLT645_RX, "duplicate event ID");
+_Static_assert(EDGE_EVT_BOARD_TIMER0 != EDGE_EVT_DLMS_RX, "duplicate event ID");
+_Static_assert(EDGE_EVT_BOARD_TIMER0 != EDGE_EVT_RELAY_CHANGED, "duplicate event ID");
 _Static_assert(EDGE_EVT_DLT645_RX != EDGE_EVT_DLMS_RX, "duplicate event ID");
 _Static_assert(EDGE_EVT_DLT645_RX != EDGE_EVT_RELAY_CHANGED, "duplicate event ID");
 _Static_assert(EDGE_EVT_DLMS_RX != EDGE_EVT_RELAY_CHANGED, "duplicate event ID");
