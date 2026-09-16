fc67c744957bc7d354d63b8727d187da3c5bf055
cautonwong <992878191@qq.com>
2026-09-16 10:48:26 +0800

    fix: correct 32-bit module ABI size assertion

--- edge_module/include/edge/module.h
+++ edge_module/include/edge/module.h
@@ -48,7 +48,7 @@ typedef struct edge_module {
 
 #ifdef EDGE_TARGET_ARM32
 _Static_assert(sizeof(void *) == 4u, "embedded ABI requires 32-bit pointers");
-_Static_assert(sizeof(edge_module_t) == 32u, "edge_module_t ABI changed for 32-bit target");
+_Static_assert(sizeof(edge_module_t) == 36u, "edge_module_t ABI changed for 32-bit target");
 #endif
 
 static inline void *edge_module_data(edge_module_t *self)
