7cd23cee9720b02a906b6dd369bfa647617dc8c0
cautonwong <992878191@qq.com>
2026-09-16 14:39:21 +0800

    style: format scheduler tests

--- tests/test_sys.c
+++ tests/test_sys.c
@@ -203,8 +203,11 @@ static void test_event_budget_and_stats(void **state) {
     assert_int_equal(edge_sys_set_event_budget(&sys, 2u), EDGE_OK);
     assert_int_equal(edge_sys_subscribe(&sys, EDGE_EVT_UART0_RX, &a.module), EDGE_OK);
     assert_int_equal(edge_sys_start(&sys), EDGE_OK);
-    for (int i = 0; i < 5; ++i)
-        assert_int_equal(edge_event_push_isr(&queue, &(edge_event_t){.id = EDGE_EVT_UART0_RX}), EDGE_OK);
+    for (int i = 0; i < 5; ++i) {
+        assert_int_equal(edge_event_push_isr(&queue,
+                                             &(edge_event_t){.id = EDGE_EVT_UART0_RX}),
+                         EDGE_OK);
+    }
     assert_int_equal(edge_sys_dispatch_events(&sys), EDGE_OK);
     assert_int_equal(a.event_count, 2);
     assert_int_equal(edge_event_count(&queue), 3u);
