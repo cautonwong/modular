bd57d4dea7e63b537e9688052fe2a1d1851c7d5b
cautonwong <992878191@qq.com>
2026-09-16 09:32:57 +0800

    refactor: make event sink clock-aware and bounded

--- include/edge/event.h
+++ include/edge/event.h
@@ -3,6 +3,11 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include "module.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 typedef struct edge_event {
     uint32_t id;
@@ -12,6 +17,8 @@ typedef struct edge_event {
     uint64_t timestamp;
 } edge_event_t;
 
+_Static_assert(sizeof(edge_event_t) == 24u, "edge_event_t ABI changed");
+
 typedef struct edge_event_queue {
     edge_event_t *items;
     uint32_t capacity;
@@ -20,19 +27,27 @@ typedef struct edge_event_queue {
     volatile uint32_t dropped;
 } edge_event_queue_t;
 
-typedef int (*edge_event_sink_fn)(const edge_event_t *event, void *arg);
+typedef uint64_t (*edge_event_clock_fn)(void *self);
 
 typedef struct edge_event_sink {
-    edge_event_sink_fn push_isr;
-    void *arg;
+    edge_event_queue_t *queue;
+    edge_event_clock_fn monotonic_ticks;
+    void *clock_self;
 } edge_event_sink_t;
 
-int edge_event_queue_init(edge_event_queue_t *queue, edge_event_t *items,
-                          uint32_t capacity);
-int edge_event_push_isr(edge_event_queue_t *queue, const edge_event_t *event);
-int edge_event_pop(edge_event_queue_t *queue, edge_event_t *event);
+edge_status_t edge_event_queue_init(edge_event_queue_t *queue,
+                                    edge_event_t *storage,
+                                    uint32_t capacity);
+edge_status_t edge_event_push_isr(edge_event_queue_t *queue,
+                                  const edge_event_t *event);
+edge_status_t edge_event_sink_push_isr(edge_event_sink_t *sink,
+                                       const edge_event_t *event);
+edge_status_t edge_event_pop(edge_event_queue_t *queue, edge_event_t *event);
 size_t edge_event_count(const edge_event_queue_t *queue);
 uint32_t edge_event_dropped(const edge_event_queue_t *queue);
-int edge_event_queue_sink_isr(const edge_event_t *event, void *arg);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
