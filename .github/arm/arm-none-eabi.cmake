cd74d1e156e6b3ca6b2f75c8b9c1221c7845270f
cautonwong <992878191@qq.com>
2026-09-16 10:40:46 +0800

    build: let CMake validate ARM compiler normally

--- .github/arm/arm-none-eabi.cmake
+++ .github/arm/arm-none-eabi.cmake
@@ -5,6 +5,6 @@ set(CMAKE_C_COMPILER arm-none-eabi-gcc CACHE FILEPATH "ARM GCC compiler")
 set(CMAKE_OBJCOPY arm-none-eabi-objcopy CACHE FILEPATH "ARM objcopy")
 set(CMAKE_SIZE arm-none-eabi-size CACHE FILEPATH "ARM size")
 
+# No executable can run on the build host; CMake must use a static-library
+# try_compile for compiler/toolchain detection.
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
-
-set(CMAKE_C_COMPILER_WORKS TRUE CACHE BOOL "arm-none-eabi-gcc is a cross compiler" FORCE)
