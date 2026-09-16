924b2b1c14ec876d4c50015a331a278d7076e8e0
cautonwong <992878191@qq.com>
2026-09-16 10:59:06 +0800

    test: remove tautological architecture assertion

--- tests/test_architecture.c
+++ tests/test_architecture.c
@@ -12,7 +12,6 @@ static void test_public_contracts(void **state)
     (void)state;
     assert_true(sizeof(edge_event_t) == 24u);
     assert_true(sizeof(edge_status_t) >= sizeof(int));
-    assert_true(EDGE_OK == 0);
     assert_true(EDGE_EINVAL < 0);
 }
 
